32b8194e342fe2baf7f57f87bdc9467ce2a57661
lucas199706 <40581941+lucas199706@users.noreply.github.com>
2020-08-03 20:57:50 -0400

    alteração

--- lib/shared/repositories/user_repository.dart
+++ lib/shared/repositories/user_repository.dart
@@ -7,7 +7,55 @@ class UserRepository {
   UserRepository(this.dio);
 
   Future<List<UsuarioModel>> getAllUsers() async {
-    var response = await dio.request("/listar",
+    var response = await dio.request("/listar_all_users",
+        options: Options(
+          headers: {"Content-Type": "application/json"},
+        ));
+    List<UsuarioModel> list = [];
+    for (var json in (response.data[0] as List)) {
+      UsuarioModel model = UsuarioModel(email: json['email']);
+      list.add(model);
+    }
+
+    return list;
+  }
+
+  Future<List<UsuarioModel>> getOneUser(idUserToken) async {
+    var response = await dio.request("/lista_user",
+        data: {"token": idUserToken},
+        options: Options(
+          headers: {"Content-Type": "application/json"},
+        ));
+    List<UsuarioModel> list = [];
+    for (var json in (response.data[0] as List)) {
+      UsuarioModel model = UsuarioModel(email: json['email']);
+      list.add(model);
+    }
+
+    return list;
+  }
+
+  Future<List<UsuarioModel>> loginUsuario(email, senha) async {
+    var response = await dio.request("/login",
+        data: {
+          "email": email,
+          "senha": senha,
+        },
+        options: Options(
+          headers: {"Content-Type": "application/json"},
+        ));
+    List<UsuarioModel> list = [];
+    for (var json in (response.data[0] as List)) {
+      UsuarioModel model = UsuarioModel(email: json['email']);
+      list.add(model);
+    }
+
+    return list;
+  }
+
+  Future<List<UsuarioModel>> cadastrarUsuario() async {
+    var response = await dio.request("/cadastrar_usuario",
+        data: {},
         options: Options(
           headers: {"Content-Type": "application/json"},
         ));
